1445d51430659d51209eb994bdc010b97b307745
april sikes <aprilsikes@gmail.com>
2016-04-05 19:17:45 -0600

    deploying all orgs

--- js/services/orgs_service.js
+++ js/services/orgs_service.js
@@ -1,11 +1,11 @@
 app.factory('OrgsService', function ($http) {
   return {
     all: function() {
-      // return $http.get('https://carpool-api.herokuapp.com/orgs', {method: "jsonp"}).then(function (orgs) {
-        return $http.get("http://localhost:3000/orgs");
+      return $http.get('https://carpool-api.herokuapp.com/orgs', {method: "jsonp"}).then(function (orgs) {
+        // return $http.get("http://localhost:3000/orgs");
         console.log(orgs);
         console.log(orgs.data);
-      // })
+      })
     },
   }
 })
