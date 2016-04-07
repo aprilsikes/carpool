ac358db58eb5d8f57bde74c2653fe4b556bf3d50
april sikes <aprilsikes@gmail.com>
2016-04-06 20:39:08 -0600

    showing groups

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -1,6 +1,7 @@
 app.controller('MainController', ['$scope', '$http', 'OrgsService', '$routeParams', function ($scope, $http, OrgsService, $routeParams) {
 
   $scope.orgs;
+  $scope.groups;
   $scope.singleOrg;
 
   OrgsService.all().then(function (orgs) {
@@ -8,13 +9,30 @@ app.controller('MainController', ['$scope', '$http', 'OrgsService', '$routeParam
     console.log(orgs);
     console.log('*******');
     console.log(orgs.data);
-    $scope.orgs = orgs.data
+    $scope.orgs = orgs.data;
   })
 
-  the_id = $routeParams.id
-  OrgsService.oneOrg(the_id).then(function (singleOrg) {
-    console.log(singleOrg);
-    $scope.singleOrg = singleOrg.data;
+  the_id = $routeParams.id;
+  OrgsService.oneOrg(the_id).then(function (payload) {
+    console.log(payload.data);
+    $scope.singleOrg = payload.data['singleOrg'];
+    switch (payload.data['singleOrg'].category) {
+      case 'School':
+        $scope.groupValue = "Groups";
+        break;
+      case 'Sports':
+        $scope.groupValue = "Teams";
+        break;
+      case 'Scouts':
+        $scope.groupValue = "Troops";
+        break;
+      case 'Arts':
+        $scope.groupValue = "Classes";
+        break;
+      default:
+        $scope.groupValue = "Groups";
+    }
+    $scope.groups = payload.data['groups'];
   })
 
 }])
